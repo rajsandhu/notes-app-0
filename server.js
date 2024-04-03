2378c5ee8618df169e660ae12a161fb3eb2f1295
Raj Sandhu <rajsandhu@gmail.com>
2024-04-03 19:05:14 +0200

    install nodemon for faster development

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+// if PORT in the .env is not set, PORT defaults to 3000
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(PORT, () => {
+    console.log(`server running on ${PORT}`)
+})
