55231f8151eb68ad6ec42e777361a6959509dc25
Raj Sandhu <rajsandhu@gmail.com>
2024-04-03 19:11:02 +0200

    install ejs for templating

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 // if PORT in the .env is not set, PORT defaults to 3000
 const PORT = process.env.PORT || 3000
 
+app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
